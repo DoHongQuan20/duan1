73beff2ed634e06f342f56f45318a7c36749eab0
Do Hong Quan <quandhpd08005@fpt.edu.vn>
2023-11-10 12:07:43 +0700

    duan

---
+++ duan/view/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css" />
+  <script src="js/slider.js"></script>
+
+</head>
+
+<body>
+  <div class="container-ner">
+    <div class="header">
+      <div class="logo"><img src="./img/logo.jpg" alt="" /></div>
+      <div class="search-container">
+        <input type="text" placeholder="Search..." />
+        <button type="submit" class="btn btn-danger">Search</button>
+      </div>
+
+      <div class="taikhoan">
+        <ul class="list-inline">
+          <li><a href="#">Đăng nhập</a></li>
+          <li><a href="#">Đăng ký</a></li>
+          <li><a href="#"><i class="fas fa-shopping-cart"></i> Giỏ hàng</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- MENU -->
+    <nav>
+      <div class="container">
+        <div class="left-boxes">
+          <ul>
+            <li><a href="#"><i class="fas fa-archive"></i> Danh sách sản phẩm</a></li>
+          </ul>
+        </div>
+
+        <div class="right-boxes">
+          <ul>
+            <li><a href="index.php"><i class="fas fa-home"></i>Trang Chủ</a></li>
+            <li><a href="index.php?act=sanpham">Sản Phẩm</a></li>
+            <li><a href="index.php?act=gioithieu">Giới Thiệu</a></li>
+            <li><a href="index.php?act=lienhe">Liên Hệ</a></li>
+          </ul>
+        </div>
+      </div>
+      <hr class="horizontal-line" />
+    </nav>
